d690152de306be4a1d351774fc85dc52164e0af1
Dan Habot <dan.habot@gmail.com>
2023-10-14 00:57:56 -0400

    started work on services

--- api/src/services/files/files.ts
+++ api/src/services/files/files.ts
@@ -6,15 +6,15 @@ import type {
 
 import { db } from 'src/lib/db'
 
-export const files: QueryResolvers['files'] = () => {
-  return db.file.findMany()
-}
+// export const files: QueryResolvers['files'] = () => {
+//   return db.file.findMany()
+// }
 
-export const file: QueryResolvers['file'] = ({ id }) => {
-  return db.file.findUnique({
-    where: { id },
-  })
-}
+// export const file: QueryResolvers['file'] = ({ id }) => {
+//   return db.file.findUnique({
+//     where: { id },
+//   })
+// }
 
 export const createFile: MutationResolvers['createFile'] = ({ input }) => {
   return db.file.create({
@@ -22,21 +22,21 @@ export const createFile: MutationResolvers['createFile'] = ({ input }) => {
   })
 }
 
-export const updateFile: MutationResolvers['updateFile'] = ({ id, input }) => {
-  return db.file.update({
-    data: input,
-    where: { id },
-  })
-}
+// export const updateFile: MutationResolvers['updateFile'] = ({ id, input }) => {
+//   return db.file.update({
+//     data: input,
+//     where: { id },
+//   })
+// }
 
-export const deleteFile: MutationResolvers['deleteFile'] = ({ id }) => {
-  return db.file.delete({
-    where: { id },
-  })
-}
+// export const deleteFile: MutationResolvers['deleteFile'] = ({ id }) => {
+//   return db.file.delete({
+//     where: { id },
+//   })
+// }
 
-export const File: FileRelationResolvers = {
-  thread: (_obj, { root }) => {
-    return db.file.findUnique({ where: { id: root?.id } }).thread()
-  },
-}
+// export const File: FileRelationResolvers = {
+//   thread: (_obj, { root }) => {
+//     return db.file.findUnique({ where: { id: root?.id } }).thread()
+//   },
+// }
--- api/src/services/publicKeys/publicKeys.ts
+++ api/src/services/publicKeys/publicKeys.ts
@@ -5,45 +5,105 @@ import type {
 } from 'types/graphql'
 
 import { db } from 'src/lib/db'
+import * as openpgp from 'openpgp'
 
 export const publicKeys: QueryResolvers['publicKeys'] = () => {
   return db.publicKey.findMany()
 }
 
-export const publicKey: QueryResolvers['publicKey'] = ({ id }) => {
+export const publicKey: QueryResolvers['publicKey'] = ({ keyId }) => {
   return db.publicKey.findUnique({
-    where: { id },
+    where: { keyId },
   })
 }
 
-export const createPublicKey: MutationResolvers['createPublicKey'] = ({
+export const createPublicKey: MutationResolvers['createPublicKey'] = async ({
   input,
 }) => {
+  const newKey = await openpgp.readKey({
+    armoredKey: input.armoredKey,
+  })
+
+  if (input.sponsorArmoredSignature) {
+    const signature = await openpgp.readSignature({
+      armoredSignature: input.sponsorArmoredSignature,
+    })
+
+    const sponsors = await db.publicKey.findMany({
+      where: {
+        OR: signature.getSigningKeyIDs().map((x) => ({ keyId: x.toHex() })),
+      },
+    })
+
+    if (!sponsors.some((x) => x.approved)) {
+      throw 'Sponsor is not approved'
+    }
+
+    if (!sponsors.some((x) => x.revoked)) {
+      throw 'Sponsor is revoked'
+    }
+
+    const verified = await openpgp.verify({
+      verificationKeys: await Promise.all(
+        sponsors.map((x) =>
+          openpgp.readKey({
+            armoredKey: x.armoredKey,
+          })
+        )
+      ),
+      message: await openpgp.createCleartextMessage({
+        text: input.armoredKey,
+      }),
+      signature: input.sponsorArmoredSignature,
+    })
+
+    if (!verified) {
+      throw "Signature doesn't match"
+    }
+  }
+
+  const { user } = await newKey.getPrimaryUser()
+
   return db.publicKey.create({
-    data: input,
+    data: {
+      armoredKey: input.armoredKey,
+      comment: user.userID.comment,
+      email: user.userID.email,
+      finger: newKey.getFingerprint(),
+      sponsorArmoredSignature: input.sponsorArmoredSignature,
+      keyId: newKey.getKeyID().toHex(),
+      name: user.userID.name,
+    },
   })
 }
 
-export const updatePublicKey: MutationResolvers['updatePublicKey'] = ({
-  id,
+export const updatePublicKey: MutationResolvers['updatePublicKey'] = async ({
   input,
 }) => {
+  const key = await openpgp.readKey({
+    armoredKey: input.armoredKey
+  })
   return db.publicKey.update({
-    data: input,
-    where: { id },
+    data: {
+      armoredKey: input.armoredKey,
+      revoked: await key.isRevoked(),
+    },
+    where: { 
+      keyId: key.getKeyID().toHex()
+    },
   })
 }
 
-export const deletePublicKey: MutationResolvers['deletePublicKey'] = ({
-  id,
-}) => {
-  return db.publicKey.delete({
-    where: { id },
-  })
-}
+// export const deletePublicKey: MutationResolvers['deletePublicKey'] = ({
+//   id,
+// }) => {
+//   return db.publicKey.delete({
+//     where: { id },
+//   })
+// }
 
 export const PublicKey: PublicKeyRelationResolvers = {
   threads: (_obj, { root }) => {
-    return db.publicKey.findUnique({ where: { id: root?.id } }).threads()
+    return db.publicKey.findUnique({ where: { keyId: root.keyId } }).threads()
   },
 }
