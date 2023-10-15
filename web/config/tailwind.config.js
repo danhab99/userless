c64ed29d3e0fcbfe9cdb4f5b64c12372a7b9964c
Dan Habot <dan.habot@gmail.com>
2023-10-14 20:01:52 -0400

    configured tailwindcss

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        "card": "#cad470",
+        "background": "#70c0d4",
+        "username": "#a970d4",
+        "hash": "#222"
+      }
+    },
+  },
+  plugins: [],
+}
+
