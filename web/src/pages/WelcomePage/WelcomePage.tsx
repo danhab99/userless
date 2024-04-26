99a7bc615990c7af5d69ec73431f9e41e6186502
Dan Habot <dan.habot@gmail.com>
2024-04-26 00:10:44 -0400

    add

--- web/src/pages/WelcomePage/WelcomePage.tsx
+++ web/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import PostThread from 'src/components/PostThread/PostThread'
+import ThreadsCell from 'src/components/ThreadsCell'
 
 const WelcomePage = () => {
   return (
@@ -13,6 +15,12 @@ const WelcomePage = () => {
           based access control.
         </p>
       </header>
+
+      <div className="flex flex-row justify-center">
+        <PostThread />
+      </div>
+
+      <ThreadsCell />
     </>
   )
 }
