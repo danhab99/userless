60c2019c7a959e4f3945229ca361a38278ddd6b5
Dan Habot <dan.habot@gmail.com>
2024-04-28 21:12:24 -0400

    config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,26 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
+import possibleTypes from 'src/graphql/possibleTypes'
+
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 import KeyContextProvider from './components/KeyContext/KeyContext'
 
 import './index.css'
 
+const graphQLClientConfig = {
+  cacheConfig: {
+    possibleTypes: possibleTypes.possibleTypes,
+  },
+}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
+      <RedwoodApolloProvider graphQLClientConfig={graphQLClientConfig}>
         <KeyContextProvider>
-        <Routes />
+          <Routes />
         </KeyContextProvider>
       </RedwoodApolloProvider>
     </RedwoodProvider>
