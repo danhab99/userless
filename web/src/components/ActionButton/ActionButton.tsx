51b4af12daf79213797347c1d5441f6c9f2b7a58
Dan Habot <dan.habot@gmail.com>
2024-07-05 15:13:45 -0400

    optional

--- web/src/components/ActionButton/ActionButton.tsx
+++ web/src/components/ActionButton/ActionButton.tsx
@@ -1,12 +1,15 @@
 export type ActionButtonProps = {
   color?: string
   label: string
-  onClick: () => void
+  onClick?: () => void
 }
 
 const ActionButton = (props: ActionButtonProps) => {
   return (
-    <a className={`text-xs text-green-700`} onClick={props.onClick}>
+    <a
+      className={`${props.color ?? 'text-green-700'}`}
+      onClick={props.onClick}
+    >
       [{props.label}]
     </a>
   )
