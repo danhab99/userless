b62493c5653ebf1ff7484d9ec48503eda284c721
Dan Habot <dan.habot@gmail.com>
2024-08-24 00:42:26 -0400

    fullscreen option

--- next/layouts/centered.tsx
+++ next/layouts/centered.tsx
@@ -1,6 +1,14 @@
-export function CenteredLayout(props: React.PropsWithChildren) {
+export type CenteredLayoutProps = {
+  fullscreen?: boolean;
+};
+
+export function CenteredLayout(
+  props: React.PropsWithChildren<CenteredLayoutProps>,
+) {
   return (
-    <div className="centered lg:items-center md:items-center h-screen">
+    <div
+      className={`centered lg:items-center md:items-center ${props.fullscreen ? "h-screen" : ""}`}
+    >
       <div className="centered-widths">{props.children}</div>
     </div>
   );
