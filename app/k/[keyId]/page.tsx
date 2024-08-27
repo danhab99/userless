cfa9160d436e930cb7baa4b776719044bbe1eff3
Dan Habot <dan.habot@gmail.com>
2024-08-27 02:11:04 -0400

    cleaned up headers

--- app/k/[keyId]/page.tsx
+++ app/k/[keyId]/page.tsx
@@ -3,7 +3,8 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Markdown from "react-markdown";
 import { getThreadsForThreadGroup } from "@/lib/db";
-import {ThreadGroup} from "@/components/ThreadGroup";
+import { ThreadGroup } from "@/components/ThreadGroup";
+import Centered from "@/components/Centered";
 
 const db = new PrismaClient();
 
@@ -24,31 +25,35 @@ const KeyPage = async ({ params }: KeyPageParams) => {
     notFound();
   }
 
-  const threads = await getThreadsForThreadGroup(params.keyId)
+  const threads = await getThreadsForThreadGroup(params.keyId);
 
   return (
     <>
-      <div className="px-4">
-        <div className="flexflex-col items-center border border-black bg-yellow-100">
-          <div className="p-2">
-            <h3 className="text-center">
-              {publicKey.name} {"<"}
-              {publicKey.email}
-              {">"}
-            </h3>
-            <p className="py-2">
-              <div className="markdown">
-                <Markdown>{publicKey.comment}</Markdown>
+      <Centered>
+        <div className="px-4 pt-8">
+          <div className="flexflex-col items-center bg-yellow-100">
+            <div className="p-2">
+              <h3 className="text-center">
+                {publicKey.name} {"<"}
+                {publicKey.email}
+                {">"}
+              </h3>
+              <div className="pt-2">
+                <div className="markdown no-scrollbar">
+                  <Markdown>{publicKey.comment}</Markdown>
+                </div>
               </div>
-            </p>
+            </div>
+            <pre className="h-40 w-full overflow-y-scroll bg-slate-300 p-1 text-xs">
+              {publicKey.armoredKey}
+            </pre>
           </div>
-          <pre className="h-40 w-full overflow-y-scroll bg-slate-300 p-1 text-xs">
-            {publicKey.armoredKey}
-          </pre>
         </div>
-      </div>
+      </Centered>
 
-      {threads.map((thread, i) => <ThreadGroup thread={thread} key={i} />)}
+      {threads.map((thread, i) => (
+        <ThreadGroup thread={thread} key={i} />
+      ))}
     </>
   );
 };
