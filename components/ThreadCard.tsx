2696d214686c3748dddc694b65c99db52b502dd0
Dan Habot <dan.habot@gmail.com>
2024-09-02 23:55:36 -0400

    fix

--- components/ThreadCard.tsx
+++ components/ThreadCard.tsx
@@ -12,7 +12,7 @@ import { useHasMaster, useMasterKey } from "./KeyContext";
 import ActionButton from "./ActionButton";
 import { useAsync, useAsyncFn } from "react-use";
 import * as openpgp from "openpgp";
-import {useState} from "react";
+import { useState } from "react";
 
 type ThreadCardProps = {
   thread: ThreadForThreadCard;
@@ -25,87 +25,87 @@ const ThreadCard = ({ thread }: ThreadCardProps) => {
   const [FullTB, showFull] = useToggleButton(false);
   const [refresh, setRefresh] = useState(false);
 
-  function Controls() {
-    const master = useMasterKey();
+  const master = useMasterKey();
 
-    const policy = useAsync(async () => {
-      if (master) {
-        const resp = await fetch(`/t/${thread.hash}/policy`);
-        const policyTxt = await resp.text();
-        const message = await openpgp.readMessage({
-          armoredMessage: policyTxt,
-        });
-        const msg = await openpgp.decrypt({
-          message,
-          decryptionKeys: [master],
-        });
-
-        const policyRaw = msg.data.toString();
-        return JSON.parse(policyRaw) as ThreadPolicy;
-      }
-    }, [master, thread, refresh]);
-
-    const change = async (policy: Partial<ThreadPolicy>) => {
-      if (master) {
-        const packet = await openpgp.sign({
-          message: await openpgp.createCleartextMessage({
-            text: JSON.stringify(policy),
-          }),
-          signingKeys: [master],
-        });
-
-        const resp = await fetch(`/t/${thread.hash}/policy`, {
-          method: "PATCH",
-          body: packet,
-        });
-
-        setRefresh(x => !x);
-
-        return resp.ok;
-      }
-      return false;
-    };
-
-    const [{ loading: deleting }, doDelete] = useAsyncFn(() => {
-      return change({
-        visible: false,
+  const policy = useAsync(async () => {
+    if (master) {
+      const resp = await fetch(`/t/${thread.hash}/policy`);
+      const policyTxt = await resp.text();
+      const message = await openpgp.readMessage({
+        armoredMessage: policyTxt,
       });
-    });
-
-    const [{ loading: disablingReplies }, disableReplies] = useAsyncFn(() => {
-      return change({
-        acceptsReplies: !policy.value?.acceptsReplies,
+      const msg = await openpgp.decrypt({
+        message,
+        decryptionKeys: [master],
       });
-    });
 
-    return (
-      <div className="text-xs">
-        <ReplyTB trueLabel="Hide reply" falseLabel="Reply" />
-        <SourceTB trueLabel="Hide source" falseLabel="Source" />
-        <FullTB trueLabel="Less" falseLabel="More" />
-        {master ? (
-          <>
-            <ActionButton
-              color="text-red-500"
-              label={deleting ? "Deleting" : "Delete"}
-              onClick={doDelete}
-            />
-            <ActionButton
-              color="text-red-500"
-              label={
-                disablingReplies
-                  ? "Changing..."
-                  : policy.value?.acceptsReplies
-                    ? "Disable replies"
-                    : "Enable replies"
-              }
-              onClick={disableReplies}
-            />
-          </>
-        ) : null}
-      </div>
-    );
-  }
+      const policyRaw = msg.data.toString();
+      return JSON.parse(policyRaw) as ThreadPolicy;
+    }
+  }, [master, thread, refresh]);
+
+  const change = async (policy: Partial<ThreadPolicy>) => {
+    if (master) {
+      const packet = await openpgp.sign({
+        message: await openpgp.createCleartextMessage({
+          text: JSON.stringify(policy),
+        }),
+        signingKeys: [master],
+      });
+
+      const resp = await fetch(`/t/${thread.hash}/policy`, {
+        method: "PATCH",
+        body: packet,
+      });
+
+      await new Promise(r => setTimeout(r, 50))
+
+      setRefresh((x) => !x);
+
+      return resp.ok;
+    }
+    return false;
+  };
+
+  const [{ loading: deleting }, doDelete] = useAsyncFn(() => {
+    return change({
+      visible: false,
+    });
+  });
+
+  const [{ loading: disablingReplies }, disableReplies] = useAsyncFn(() => {
+    return change({
+      acceptsReplies: !policy.value?.acceptsReplies,
+    });
+  });
+
+  const controls = (
+    <div className="text-xs">
+      <ReplyTB trueLabel="Hide reply" falseLabel="Reply" />
+      <SourceTB trueLabel="Hide source" falseLabel="Source" />
+      <FullTB trueLabel="Less" falseLabel="More" />
+      {master ? (
+        <>
+          <ActionButton
+            color="text-red-500"
+            label={deleting ? "Deleting" : "Delete"}
+            onClick={doDelete}
+          />
+          <ActionButton
+            color="text-red-500"
+            label={
+              disablingReplies
+                ? "Changing..."
+                : policy.value?.acceptsReplies
+                  ? "Disable replies"
+                  : "Enable replies"
+            }
+            onClick={disableReplies}
+          />
+        </>
+      ) : null}
+    </div>
+  );
 
   const mailtoLink = mailto({
     to: thread.signedBy.email,
@@ -135,11 +135,11 @@ const ThreadCard = ({ thread }: ThreadCardProps) => {
         </Link>{" "}
         <SigVerify thread={thread} />
       </p>
-      <Controls />
+      {controls}
       <div className={showFull ? "h-full" : "max-h-96 overflow-y-auto"}>
         <ThreadBody thread={thread as Thread} />
       </div>
-      <Controls />
+      {controls}
       {showReply ? (
         <div className="pt-4">
           <PostThread replyTo={thread} />
