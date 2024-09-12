b86b677397b0e882541a56225d32dc1f1a9147b5
Dan Habot <dan.habot@gmail.com>
2024-09-12 18:01:54 -0400

    return sig

--- app/f/[hash]/sig/route.ts
+++ app/f/[hash]/sig/route.ts
@@ -1,9 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { BUCKET, s3Client } from "@/lib/s3";
-import { redirect } from "next/navigation";
 
 export async function GET(req: NextRequest) {
   const hash = req.nextUrl.pathname.split("/")[2];
-  const url = await s3Client.presignedGetObject(BUCKET, hash + "_sig");
-  redirect(url);
+  const sig = await s3Client.getObject(BUCKET, hash + "_sig")
+  return new NextResponse(sig.read());
 }
