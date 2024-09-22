7a281487b2b0a4eabb5885ea13f2e7a626e0db70
Dan Habot <dan.habot@gmail.com>
2024-09-22 13:05:55 -0400

    do sigverify on images

--- components/ThreadBody.tsx
+++ components/ThreadBody.tsx
@@ -5,6 +5,7 @@ import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 import { Thread } from "@prisma/client";
 import { useAsync } from "react-use";
+import SigVerify from "./SigVerify";
 // import * as syntax_highlight from 'highlight.js/lib/languages/*';
 
 type ThreadBodyProps = {
@@ -30,6 +31,50 @@ const ThreadBody = (props: ThreadBodyProps) => {
       <Markdown
         remarkPlugins={[remarkGfm]}
         // rehypePlugins={[rehypeHighlight, { languages: syntax_highlight.default }]}
+        components={{
+          img: (props) => {
+            if (!props.src) {
+              return <></>;
+            }
+            const srcUrl = new URL(props.src);
+
+            const resolvedUrl = useAsync(async () => {
+              if (props.src && srcUrl.protocol === "userless") {
+                const u = new URL(window.location.href);
+                u.pathname = "/resolve";
+                u.searchParams.set("u", props.src);
+
+                const resp = await fetch(u.toString(), {
+                  cache: "force-cache",
+                  redirect: "manual",
+                });
+
+                return resp.url;
+              }
+            }, [srcUrl, props.src]);
+
+            const content = useAsync(async () => {
+              const url = resolvedUrl.value;
+              if (url) {
+                const resp = await fetch(url);
+                return resp.arrayBuffer();
+              }
+            }, [resolvedUrl.value]);
+
+            if (srcUrl.protocol === "userless") {
+              return (
+                <>
+                  <img src={resolvedUrl.value ?? ""} {...props} />
+                  {content.value ? (
+                    <SigVerify detatched content={content.value} />
+                  ) : null}
+                </>
+              );
+            } else {
+              return <img {...props} />;
+            }
+          },
+        }}
       >
         {body.value}
       </Markdown>
