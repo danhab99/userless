555e97e19a360a841d96ce921f87dcb630583433
Dan Habot <dan.habot@gmail.com>
2024-09-22 13:05:08 -0400

    resolve pk with keyid or fingerprint

--- app/k/[keyId]/armored/route.ts
+++ app/k/[keyId]/armored/route.ts
@@ -7,8 +7,17 @@ export async function HEAD(request: Request) {
   const u = new URL(request.url);
   const finger = u.pathname.split("/")[2].toLowerCase();
 
-  const res = await db.publicKey.findUnique({
-    where: { finger },
+  const res = await db.publicKey.findFirst({
+    where: { 
+      OR: [
+        {
+          finger: finger,
+        },
+        {
+          keyId: finger,
+        }
+      ]
+    },
     select: {
       id: true,
     },
@@ -23,8 +32,17 @@ export async function GET(request: Request) {
   const u = new URL(request.url);
   const finger = u.pathname.split("/")[2].toLowerCase();
 
-  const publicKey = await db.publicKey.findUnique({
-    where: { finger },
+  const publicKey = await db.publicKey.findFirst({
+    where: { 
+      OR: [
+        {
+          finger: finger,
+        },
+        {
+          keyId: finger,
+        }
+      ]
+    },
     select: {
       armoredKey: true,
     },
