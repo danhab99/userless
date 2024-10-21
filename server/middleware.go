4f61ff87fffbf525ad7dbea21b9417005138d6f1
Dan Habot <dan.habot@gmail.com>
2024-10-20 21:56:16 -0500

    fetches

--- server/middleware.go
+++ server/middleware.go
@@ -1,17 +1,59 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"userless/server/prisma/db"
 
-func hashMiddleware(uc UserlessCtx) func(ctx *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
+
+func threadMiddleware(uc UserlessCtx) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-		ctx.Set
+		hash := ctx.Params.ByName("hash")
+
+		thread, err := uc.client.Thread.FindFirst(
+			db.Thread.Hash.Equals(hash),
+		).Exec(context.Background())
+
+		if err != nil {
+			panic(err)
+		}
+
+		ctx.Set("thread", thread)
+		ctx.Next()
 	}
 }
 
 func keyMiddleware(uc UserlessCtx) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		id := ctx.Params.ByName("id")
 
+		key, err := uc.client.PublicKey.FindFirst(
+			db.PublicKey.KeyID.Equals(id),
+		).Exec(context.Background())
+
+		if err != nil {
+			panic(err)
+		}
+
+		ctx.Set("key", key)
+		ctx.Next()
+	}
 }
 
 func fileMiddleware(uc UserlessCtx) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		hash := ctx.Params.ByName("hash")
 
+		file, err := uc.client.File.FindFirst(
+			db.File.Hash.Equals(hash),
+		).Exec(context.Background())
+
+		if err != nil {
+			panic(err)
+		}
+
+		ctx.Set("file", file)
+		ctx.Next()
+	}
 }
