de3a9944de824e85f5ec24547066829581f78907
Dan Habot <dan.habot@gmail.com>
2024-12-09 23:39:59 -0500

    change

--- server/main.go
+++ server/main.go
@@ -17,20 +17,20 @@ func main() {
 	route.POST("/upload", uploadHandler(ctx))
 
 	threadGroup := route.Group("/thread/:hash", threadMiddleware(ctx))
-	threadGroup.GET("/", getThead(ctx))
+	threadGroup.GET("", getThead(ctx))
 	threadGroup.GET("/replies", getThreadReplies(ctx))
 	threadGroup.GET("/policy", getThreadPolicy(ctx))
 	threadGroup.PATCH("/policy", patchThreadPolicy(ctx))
 
 	keyGroup := route.Group("/key/:id", keyMiddleware(ctx))
-	keyGroup.GET("/armored", getKey(ctx))
+	keyGroup.GET("", getKey(ctx))
 	keyGroup.GET("/files", getKeyFiles(ctx))
 	keyGroup.GET("/threads", getKeyThreads(ctx))
 	keyGroup.GET("/policy", getKeyPolicy(ctx))
 	keyGroup.PATCH("/policy", patchKeyPolicy(ctx))
 
 	fileGroup := route.Group("/file/:hash", fileMiddleware(ctx))
-	fileGroup.GET("/", getFile(ctx, ""))
+	fileGroup.GET("", getFile(ctx, ""))
 	fileGroup.GET("/sig", getFile(ctx, "_sig"))
 
 	route.Run("0.0.0.0:9000")
