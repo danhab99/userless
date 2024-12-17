55857bc74bc5a325d40f4587011166c56cf16ace
Dan Habot <dan.habot@gmail.com>
2024-12-16 23:26:13 -0500

    more methods

--- api-wrapper/key.ts
+++ api-wrapper/key.ts
@@ -3,11 +3,13 @@ import { Thread } from "./thread";
 import { parse } from "smol-toml";
 
 export class PublicKey extends BaseFetcher {
+  readonly armored: string;
   readonly keyId: string;
 
-  protected constructor(url: string, keyId: string) {
+  constructor(url: string, keyId: string, armored: string) {
     super(url, `key/${keyId}`);
     this.keyId = keyId;
+    this.armored = armored;
   }
 
   public async getArmored() {
--- api-wrapper/userless.ts
+++ api-wrapper/userless.ts
@@ -1,6 +1,8 @@
 import { Fetcher } from "./fetch";
 import { Banner } from "./types";
 import { Content } from "./content";
+import { Thread } from "./thread";
+import {PublicKey} from "./key";
 
 export class UserlessServer extends Fetcher {
   public async getBanner(): Promise<Banner> {
@@ -8,7 +10,13 @@ export class UserlessServer extends Fetcher {
     return new Content(ret) as Banner;
   }
 
-  public async getThread(hash: string) {
-    return this.fetch(`/thread/${hash}`);
+  public async getThread(hash: string): Promise<Thread> {
+    const resp = await this.fetch(`/thread/${hash}`);
+    return new Thread(this.url, hash, resp);
+  }
+
+  public async getKey(keyId: string): Promise<PublicKey> {
+    const resp = await this.fetch(`/key/${keyId}`)
+    return new PublicKey(this.url, keyId, resp);
   }
 }
