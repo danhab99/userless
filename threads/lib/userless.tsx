b330e7f3ffcd3b685275b228ec21ceac747d0453
Dan Habot <dan.habot@gmail.com>
2024-12-18 23:10:36 -0500

    change env

--- threads/lib/userless.tsx
+++ threads/lib/userless.tsx
@@ -1,3 +1,3 @@
 import { UserlessServer } from "api-wrapper";
 
-export const server = new UserlessServer(process.env["USERLESS_URL"] as string);
+export const server = new UserlessServer(process.env["NEXT_PUBLIC_USERLESS_URL"] as string);
