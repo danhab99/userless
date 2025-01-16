53c9db88b553efe8ea621b331806dab6eaa79d70
Dan Habot <dan.habot@gmail.com>
2025-01-15 23:26:32 -0500

    typescript

--- threads/next.config.mjs
+++ threads/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['api-wrapper'],
+  webpack: (config, { isServer }) => {
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts'],
+      '.jsx': ['.jsx', '.tsx'],
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
