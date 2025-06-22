29c5c89e8ed58ead38d022800d9c34242e234872
Dan Habot <dan.habot@gmail.com>
2025-06-22 19:22:20 -0400

    deleted old routes

--- threads/app/admin/route.ts
+++
@@ -1,7 +0,0 @@
-import {NextRequest, NextResponse} from "next/server";
-import { encryptForMasters } from "@/lib/admin";
-
-export async function GET(req: NextRequest) {
-  const packet = await encryptForMasters("if you can read this then you're an admin");
-  return new NextResponse(packet);
-}
--- threads/app/file/[hash]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-import { BUCKET, s3Client } from "@/lib/s3";
-import { redirect } from "next/navigation";
-
-export async function GET(req: NextRequest) {
-  const hash = req.nextUrl.pathname.split("/")[2];
-  const url = await s3Client.presignedGetObject(BUCKET, hash);
-  redirect(url);
-}
