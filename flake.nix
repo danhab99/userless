fd00cbdd56f8042c62f31d8bf6d79df6c39da562
Dan Habot <dan.habot@gmail.com>
2025-06-22 19:20:16 -0400

    dev env

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        lib = pkgs.lib;
+
+      in {
+        devShells = {
+          go = pkgs.mkShell {
+            packages = with pkgs; [
+              gnumake
+              go
+            ];
+
+            GO_PATH="${self.outPath}/.go";
+
+            shellHook = ''
+            zsh
+            '';
+          };
+
+          ts = pkgs.mkShell {
+            packages = with pkgs; [
+              gnumake
+              yarn
+              nodejs_22
+            ];
+
+            shellHook = ''
+            zsh
+            '';
+          };
+        };
+      });
+}
