1a2f314a9e5444293833846b294ddd2f2582262a
Dan Habot <dan.habot@gmail.com>
2026-02-27 23:59:49 -0500

    initialize redditless

---
+++ redditless/.storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+import tailwindcss from '@tailwindcss/vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/nextjs-vite",
+  "staticDirs": [
+    "../public"
+  ],
+  viteFinal: (config) => {
+    config.plugins = [tailwindcss(), ...(config.plugins ?? [])];
+    return config;
+  },
+};
+export default config;
