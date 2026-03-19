9ddd5dac2e0d7464fccdfc7801762051504aceab
Dan Habot <dan.habot@gmail.com>
2026-03-19 19:25:57 -0400

    restyled hash component

--- ui-components/src/Hash/Hash.tsx
+++ ui-components/src/Hash/Hash.tsx
@@ -1,6 +1,6 @@
 export function Hash(props: { content: string }) {
   return (
-    <span className="inline-block w-[8ch] overflow-hidden whitespace-nowrap align-middle">
+    <span className="inline-block w-[8ch] font-mono hover:w-full overflow-hidden whitespace-nowrap align-middle text-gray-800">
       {props.content}
     </span>
   );
