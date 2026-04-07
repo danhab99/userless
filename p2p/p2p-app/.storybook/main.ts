07aa21010bcce820567a90a0cc78d9b71507069e
Dan Habot <dan.habot@gmail.com>
2026-04-06 22:05:14 -0400

    storybook config

---
+++ p2p/p2p-app/.storybook/main.ts
@@ -0,0 +1,10 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-interactions'],
+  framework: '@storybook/react-vite',
+  staticDirs: ['../public'],
+}
+
+export default config
